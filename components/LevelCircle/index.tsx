3677dd656d595b48c891747dd33560adb1a0a801
codeisneverodd <codeisneverodd@gmail.com>
2023-01-20 21:25:26 +0900

    feat: change bg color statement

--- components/LevelCircle/index.tsx
+++ components/LevelCircle/index.tsx
@@ -1,14 +1,15 @@
+import { levelColors } from 'styles/colors';
 import { tw } from 'utils/tailwindMerge';
 
 type LevelCircleProps<T extends React.ElementType> = {
-  level: number;
+  level: ProbLevel;
 } & Component<T>;
 
 export function LevelCircle({ children, className, level, ...restProps }: LevelCircleProps<'div'>) {
-  const levels = [0, 1, 2, 3, 4, 5].map((l) => `bg-level-${l}`);
   return (
     <div
-      className={tw(`centered h-7 w-7 rounded-full font-bungee text-[16px] text-white`, `bg-level-${level}`, className)}
+      className={tw(`centered h-7 w-7 rounded-full font-bungee text-[16px] text-white`, className)}
+      style={{ backgroundColor: levelColors[level] }}
       {...restProps}
     >
       {level}

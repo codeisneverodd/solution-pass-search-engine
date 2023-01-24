39b4f03f1cf182200c8ede26e3f7ff18e6d217d7
codeisneverodd <codeisneverodd@gmail.com>
2023-01-24 23:49:46 +0900

    config: add img remotePattern, i18n

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+    ],
+  },
+  i18n: {
+    locales: ['ko-KR', 'en'],
+    defaultLocale: 'ko-KR',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
